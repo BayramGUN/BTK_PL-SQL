d0f1629f8c7735c7814f2d8743bfbf0e6a393d9e
BayramGUN <bayram.gun635@gmail.com>
2024-07-20 22:51:55 +0300

    Implicit Cursors use with DML (DELETE & UPDATE) Examples

--- iterative_control_statements(loops)/cursors/btk_implicit-cursors.sql
+++ iterative_control_statements(loops)/cursors/btk_implicit-cursors.sql
@@ -27,3 +27,56 @@ BEGIN
    exception when no_data_found then
         dbms_output.put_line('There is ' || sql%rowcount || ' record from exception!');
 END;
+
+-- implicit cursors with DML (Update - Delete)
+
+/*
+SELECT DEPARTMENT_ID, COUNT(*) FROM HR.EMPLOYEES2
+GROUP BY DEPARTMENT_ID;
+*/
+
+DECLARE
+    rec_emp hr.employees%rowtype;
+BEGIN
+
+    -- UPDATE EXAMPLE
+   update hr.employees2 set salary = SALARY * 1.2 where department_id = 500; 
+   
+   
+   if sql%found 
+        then
+            dbms_output.put_line(sql%rowcount || ' rows updated!');
+   else
+        dbms_output.put_line('There is ' || sql%rowcount || ' record  from sql%found!');
+   end if;
+   
+   
+   if sql%notfound 
+        then
+            dbms_output.put_line('There is ' || sql%rowcount || ' record from sql%notfound!');
+   else
+        dbms_output.put_line('There is ' || sql%rowcount || ' record from sql%notfound!');
+   end if;
+   
+   -- DELETE EXAMPLE
+   delete from hr.employees2 where department_id is null; 
+   
+   
+   if sql%found 
+        then
+            dbms_output.put_line(sql%rowcount || ' rows deleted!');
+   else
+        dbms_output.put_line('There is ' || sql%rowcount || ' record  from sql%found!');
+   end if;
+   
+   
+   if sql%notfound 
+        then
+            dbms_output.put_line('There is ' || sql%rowcount || ' record from sql%notfound!');
+   else
+        dbms_output.put_line('There is ' || sql%rowcount || ' record from sql%notfound!');
+   end if;
+   exception when no_data_found then
+        dbms_output.put_line('There is ' || sql%rowcount || ' record from exception!');
+
+END;
